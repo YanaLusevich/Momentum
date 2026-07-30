c3a19ecb4fcf76e8e8cd816bc96fd6e8fd45f636
Yana_Lusevich <preety_lunx@mail.ru>
2026-07-30 14:31:53 +0300

    "fix: fix greeting"

--- js/index.js
+++ js/index.js
@@ -33,26 +33,24 @@ function timeOfDay() {
     const date = new Date()
     const hour = date.getHours()
 
-    if(hour >= 5 && hour < 12) {
+    if(hour >= 6 && hour < 12) {
         greetingText = 'morning'
-    } else if (hour >= 12 && hour < 17) {
+    } else if (hour >= 12 && hour < 18) {
         greetingText = 'afternoon'
-    } else if (hour >= 17 && hour < 22) {
+    } else if (hour >= 18 && hour < 24) {
         greetingText = 'evening'
-    } else if (hour >= 22 && hour < 5) {
+    } else {
         greetingText = 'night'
     }
 }
 
-timeOfDay()
-
 function showGreeting() {
-
-        greeting.textContent = `Good ${greetingText}`
-        console.log(greetingText)
+    timeOfDay()
+    greeting.textContent = `Good ${greetingText}`
     
 }
-showGreeting()
+setInterval(showGreeting, 60000)
+
 
 // Local Storage
 
